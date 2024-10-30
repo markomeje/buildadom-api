70ad576dcb7ef57fbec4ea16cd0840936e2c6811
markomeje <markomejeonline@gmail.com>
2024-10-30 11:27:26 +0100

    Product Service Fixes

--- app/Services/V1/Product/ProductService.php
+++ app/Services/V1/Product/ProductService.php
@@ -39,8 +39,12 @@ class ProductService extends BaseService
   public function show($id): JsonResponse
   {
     try {
-      $product = Product::published()->with(['images', 'unit', 'category', 'store'])->find($id);
-      return responser()->send(Status::HTTP_OK, $product, 'Operation successful.');
+      $product = Product::published()->with(['images', 'unit', 'category', 'store', 'currency'])->find($id);
+      if(empty($product)) {
+        return responser()->send(Status::HTTP_NOT_FOUND, null, 'Product not found. Try again.');
+      }
+
+      return responser()->send(Status::HTTP_OK, new ProductResource($product), 'Operation successful.');
     } catch (Exception $e) {
       return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, null, $e->getMessage());
     }
@@ -54,7 +58,7 @@ class ProductService extends BaseService
   {
     try {
       $search = $request->get('query');
-      $products = Product::with('store')->published()
+      $products = Product::with(['images', 'unit', 'category', 'store', 'currency'])->published()
         ->where(function ($query) use ($search) {
           $query->where('name', 'LIKE', "%{$search}%")
             ->orWhere('description', 'LIKE', "%{$search}%")
@@ -80,20 +84,20 @@ class ProductService extends BaseService
       $max_price = $request->get('max_price');
       $product_store = $request->get('product_store');
 
-      $products = Product::query()->with(['store', 'category'])->published()
-        ->where(function ($query) use ($product_unit, $product_category, $min_price, $max_price, $product_store) {
-          $query->when($product_unit, function ($query) use ($product_unit) {
-            return $query->orWhere('product_unit_id', $product_unit);
-          })->when($product_category, function ($query) use ($product_category) {
-            return $query->orWhere('product_category_id', $product_category);
-          })->when($min_price, function ($query) use ($min_price) {
-            return $query->orWhere('price', '>=', $min_price);
-          })->when($max_price, function ($query) use ($max_price) {
-            return $query->orWhere('price', '<=', $max_price);
-          })->when($product_store, function ($query) use ($product_store) {
-            return $query->orWhere('store_id', $product_store);
-          });
-        })->get();
+      $products = Product::query()->with(['images', 'unit', 'category', 'store', 'currency'])->published()
+      ->where(function ($query) use ($product_unit, $product_category, $min_price, $max_price, $product_store) {
+        $query->when($product_unit, function ($query) use ($product_unit) {
+          return $query->orWhere('product_unit_id', $product_unit);
+        })->when($product_category, function ($query) use ($product_category) {
+          return $query->orWhere('product_category_id', $product_category);
+        })->when($min_price, function ($query) use ($min_price) {
+          return $query->orWhere('price', '>=', $min_price);
+        })->when($max_price, function ($query) use ($max_price) {
+          return $query->orWhere('price', '<=', $max_price);
+        })->when($product_store, function ($query) use ($product_store) {
+          return $query->orWhere('store_id', $product_store);
+        });
+      })->get();
 
       return responser()->send(Status::HTTP_OK, ProductResource::collection($products), 'Operation successful.');
     } catch (Exception $e) {
