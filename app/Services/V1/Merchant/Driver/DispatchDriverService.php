ed67d73ec7bdc9e2161c5a513b387d8b2c2df994
markomeje <markomejeonline@gmail.com>
2025-03-12 12:39:48 +0100

    Fix indentation

--- app/Services/V1/Merchant/Driver/DispatchDriverService.php
+++ app/Services/V1/Merchant/Driver/DispatchDriverService.php
@@ -3,8 +3,6 @@
 namespace App\Services\V1\Merchant\Driver;
 use App\Models\Driver\DispatchDriver;
 use App\Services\BaseService;
-use App\Utility\Help;
-use App\Utility\Responser;
 use App\Utility\Status;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -13,61 +11,61 @@ use Illuminate\Http\Request;
 
 class DispatchDriverService extends BaseService
 {
-  /**
-   * @param Request $request
-   * @return JsonResponse
-   */
-  public function add(Request $request): JsonResponse
-  {
-    try {
-      $driver = DispatchDriver::create([
-        'phone' => formatPhoneNumber($request->phone),
-        'user_id' => auth()->id(),
-        'firstname' => $request->firstname,
-        'lastname' => $request->lastname,
-      ]);
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function add(Request $request): JsonResponse
+    {
+        try {
+            $driver = DispatchDriver::create([
+                'phone' => formatPhoneNumber($request->phone),
+                'user_id' => auth()->id(),
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+            ]);
 
-      return responser()->send(Status::HTTP_OK, $driver, 'Operation successful.');
-    } catch (Exception $e) {
-      return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+            return responser()->send(Status::HTTP_OK, $driver, 'Operation successful.');
+        } catch (Exception $e) {
+            return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+        }
     }
-  }
 
-  /**
-   * @return JsonResponse
-   */
-  public function list(): JsonResponse
-  {
-    try {
-      $drivers = DispatchDriver::owner()->get();
-      return responser()->send(Status::HTTP_OK, $drivers, 'Operation successful.');
-    } catch (Exception $e) {
-      return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e->getMessage());
+    /**
+     * @return JsonResponse
+     */
+    public function list(): JsonResponse
+    {
+        try {
+            $drivers = DispatchDriver::owner()->get();
+            return responser()->send(Status::HTTP_OK, $drivers, 'Operation successful.');
+        } catch (Exception $e) {
+            return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e->getMessage());
+        }
     }
-  }
 
-  /**
-   * @param Request $request
-   * @return JsonResponse
-   */
-  public function update(Request $request): JsonResponse
-  {
-    try {
-      $driver = DispatchDriver::where(['id' => $request->id, 'user_id' => auth()->id()])->first();
-      if(empty($driver)) {
-        return responser()->send(Status::HTTP_NOT_FOUND, [], 'Driver not found');
-      }
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function update(Request $request): JsonResponse
+    {
+        try {
+            $driver = DispatchDriver::where(['id' => $request->id, 'user_id' => auth()->id()])->first();
+            if(empty($driver)) {
+                return responser()->send(Status::HTTP_NOT_FOUND, [], 'Driver not found');
+            }
 
-      $driver->update([
-        'phone' => formatPhoneNumber($request->phone),
-        'firstname' => $request->firstname,
-        'lastname' => $request->lastname,
-      ]);
+            $driver->update([
+                'phone' => formatPhoneNumber($request->phone),
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+            ]);
 
-      return responser()->send(Status::HTTP_OK, $driver, 'Operation successful.');
-    } catch (Exception $e) {
-      return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+            return responser()->send(Status::HTTP_OK, $driver, 'Operation successful.');
+        } catch (Exception $e) {
+            return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+        }
     }
-  }
 
 }
--- app/Services/V1/Merchant/Payment/PaymentService.php
+++ app/Services/V1/Merchant/Payment/PaymentService.php
@@ -11,18 +11,18 @@ use Illuminate\Http\Request;
 
 class PaymentService extends BaseService
 {
-  /**
-   * @param Request $request
-   * @return JsonResponse
-   */
-  public function list(Request $request): JsonResponse
-  {
-    try {
-      $payments = Payment::owner()->latest()->paginate($request->limit ?? 20);
-      return responser()->send(Status::HTTP_OK, $payments, 'Operation successful.');
-    } catch (Exception $e) {
-      return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function list(Request $request): JsonResponse
+    {
+        try {
+            $payments = Payment::owner()->latest()->paginate($request->limit ?? 20);
+            return responser()->send(Status::HTTP_OK, $payments, 'Operation successful.');
+        } catch (Exception $e) {
+            return responser()->send(Status::HTTP_INTERNAL_SERVER_ERROR, [], 'Operation failed. Try again.', $e);
+        }
     }
-  }
 
 }
